91d491198615ea3b09d0da15da97ed64ff9e9e9d
Zairi, Sofien <Sofien.Zairi@fisglobal.com>
2021-05-10 08:42:49 +0100

    Created an initial breadcrumb template ...

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,23 +1,26 @@
 import { SidebarModule } from './sidebar/sidebar.module';
-import { HeaderComponent } from './header/containers/header/header.component';
 import { HeaderModule } from './header/header.module';
-import { MatButtonModule } from '@angular/material/button';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingsMenuComponent } from './ui-elements/settings-menu/settings-menu.component';
 
 import { LayoutComponent } from './layout/layout.component';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
 
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
 import { FooterComponent } from './footer/footer.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     SettingsMenuComponent,
     LayoutComponent,
-    FooterComponent
+    FooterComponent,
+    BreadcrumbComponent
   ],
   imports: [
     CommonModule,
@@ -26,12 +29,15 @@ import { FooterComponent } from './footer/footer.component';
     MatButtonModule,
     MatIconModule,
     MatMenuModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatCardModule,
+    MatListModule
   ],
   exports: [
     SettingsMenuComponent,
     LayoutComponent,
-    FooterComponent
+    FooterComponent,
+    BreadcrumbComponent
   ]
 })
 export class SharedModule { }
