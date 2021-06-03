6f600d6837f8fe73cee71264ffc093e2778faa2e
Zairi, Sofien <Sofien.Zairi@fisglobal.com>
2021-06-03 07:59:07 +0100

    Created a user task actions menu component

---
+++ src/app/pages/user/profile/components/user-task-actions-menu/user-task-actions-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-task-actions-menu',
+  templateUrl: './user-task-actions-menu.component.html',
+  styleUrls: ['./user-task-actions-menu.component.scss']
+})
+export class UserTaskActionsMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/pages/user/profile/profile.module.ts
+++ src/app/pages/user/profile/profile.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ProfilePageComponent } from './containers/profile-page/profile-page.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { UserTasksComponent } from './components/user-tasks/user-tasks.component';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatMenuModule } from '@angular/material/menu';
+
+import { UserTaskActionsMenuComponent } from './components/user-task-actions-menu';
+import { UserTasksComponent } from './components/user-tasks';
+import { ProfilePageComponent } from './containers/profile-page';
 
 @NgModule({
   declarations: [
     ProfilePageComponent,
-    UserTasksComponent
+    UserTasksComponent,
+    UserTaskActionsMenuComponent
   ],
   imports: [
     CommonModule,
@@ -23,7 +27,8 @@ import { MatTabsModule } from '@angular/material/tabs';
     MatToolbarModule,
     MatIconModule,
     MatCardModule,
-    MatTabsModule
+    MatTabsModule,
+    MatMenuModule
   ]
 })
 export class ProfileModule { }
