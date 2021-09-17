edf18784b9a485cd02ef022cbe77bed00cdb703d
Zairi, Sofien <Sofien.Zairi@fisglobal.com>
2021-09-17 13:37:40 +0100

    - Separated the add user wizard stepper and the form parts in two different components for better look and feel ...

--- src/app/pages/user/add/components/add-user-form-part-loader/add-user-form-part-loader.component.ts
+++ src/app/pages/user/add/components/add-user-form-part-loader/add-user-form-part-loader.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { MatHorizontalStepper } from '@angular/material/stepper';
+import { Observable } from 'rxjs';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, Input, OnInit, TemplateRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-add-user-form-part-loader',
   templateUrl: './add-user-form-part-loader.component.html',
   styleUrls: ['./add-user-form-part-loader.component.scss']
 })
-export class AddUserFormPartLoaderComponent implements OnInit {
+export class AddUserFormPartLoaderComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  addAccountForm: FormGroup;
+  addUserDetailsForm: FormGroup;
+
+  stepTemplateRefs: TemplateRef<any>[];
+
+  @ViewChild('part_1') part_1: TemplateRef<any>;
+  @ViewChild('part_2') part_2: TemplateRef<any>;
+
+  @Input('stepper') stepper: MatHorizontalStepper;
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
 
   ngOnInit(): void {
+    this.addAccountForm = this.fb.group({
+      username: ['', Validators.required]
+    });
+
+    this.addUserDetailsForm = this.fb.group({
+      firstname: ['', Validators.required]
+    });
+
   }
 
+  ngAfterViewInit(): void {
+    setTimeout(
+      () => this.stepTemplateRefs = [this.part_1, this.part_2]
+    )
+  }
 }
--- src/app/pages/user/add/containers/user-add-page/user-add-page.component.ts
+++ src/app/pages/user/add/containers/user-add-page/user-add-page.component.ts
@@ -1,32 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { AddUserFormStepperComponent } from './../../components/add-user-form-stepper/add-user-form-stepper.component';
+import { FormGroup } from '@angular/forms';
+import { MatHorizontalStepper } from '@angular/material/stepper';
+import { Observable } from 'rxjs';
+import { Component, OnInit, TemplateRef, ViewChild, AfterViewInit, QueryList } from '@angular/core';
 
 @Component({
   selector: 'app-user-add-page',
   templateUrl: './user-add-page.component.html',
   styleUrls: ['./user-add-page.component.scss']
 })
-export class UserAddPageComponent implements OnInit {
-
-  lessons: string[];
-  totaleEstimates = 10;
-  ctx = {estimate: this.totaleEstimates};
+export class UserAddPageComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
+  stepper: MatHorizontalStepper;
+  @ViewChild(AddUserFormStepperComponent) stepperComponent : AddUserFormStepperComponent;
+
+  @ViewChild('addAccountForm') accountForm : FormGroup;
+  @ViewChild('addUserDetailsForm') userDetailsForm : FormGroup;
+
   ngOnInit(): void {
 
-    this.lessons = ['lesson-1', 'lesson-2'];
   }
 
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.stepper = this.stepperComponent.stepper;
+      console.log("UserAddPageComponent :: stepperComponent value: ", this.stepperComponent);
+      console.log("UserAddPageComponent :: Stepper value: ", this.stepper);
+    });
 
-  /* Fake methods for convenience ... */
-
-  login() {
-    console.log('Login');
-  }
-
-  signUp() {
-    console.log('Sign Up');
   }
 
 }
