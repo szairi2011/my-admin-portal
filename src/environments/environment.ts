7fc4fc1fafb806df9505262e413ca74edb2dba2e
Zairi, Sofien <Sofien.Zairi@fisglobal.com>
2021-12-13 09:55:48 +0100

    - Make IP env variable dynamic

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  user_task_api_url: 'http://localhost:3000/tasks/',
-  user_info_api_url: 'http://localhost:3000/users/'
+  user_task_api_url: 'http://' + window.location.hostname + ':3000/tasks/',
+  // user_info_api_url: 'http://localhost:3000/users/'
+  user_info_api_url: 'http://' + window.location.hostname + ':3000/users/'
 };
 
 /*
