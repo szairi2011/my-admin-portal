f29d5874b18bfde47277e5777ecc88f16a1a6db1
Zairi <Sofien.Zairi@FISGLOBAL.COM>
2022-04-06 10:01:37 +0100

    Cretated a packaged version of the helm chart

--- src/assets/env.template.js
+++ src/assets/env.template.js
@@ -2,6 +2,7 @@
   window['env'] = window['env'] || {};
 
   // Environment variables
+  // Check example under -- https://pumpingco.de/blog/environment-variables-angular-docker/
   window['env']['tasks_backend_url_suffix'] = '${TASKS_BACKEND_URL_SUFFIX}';
   window['env']['users_backend_url_suffix'] = '${USERS_BACKEND_URL_SUFFIX}';
   window['env']['app_title'] = '${FRONTEND_NAME}' + ' v' + '${APP_FRONTEND_VERSION}';
